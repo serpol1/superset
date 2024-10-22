59b12542ea84d3acc3fb84174f2085ddb83303fe
Sergei Polshkov <polshkov1@mail.ru>
2024-10-22 12:54:56 +0300

    fix_4.0.1 - 9 high level vulnerabilities were removed

--- superset/initialization/__init__.py
+++ superset/initialization/__init__.py
@@ -368,8 +368,8 @@ class SupersetAppInitializer:  # pylint: disable=too-many-public-methods
             category_label=__("Security"),
             icon="fa-list-ol",
             menu_cond=lambda: (
-                    self.config["FAB_ADD_SECURITY_VIEWS"]
-                    and self.config["SUPERSET_LOG_VIEW"]
+                self.config["FAB_ADD_SECURITY_VIEWS"]
+                and self.config["SUPERSET_LOG_VIEW"]
             ),
         )
         appbuilder.add_api(SecurityRestApi)
@@ -446,9 +446,9 @@ class SupersetAppInitializer:  # pylint: disable=too-many-public-methods
 
         if self.config["SECRET_KEY"] == CHANGE_ME_SECRET_KEY:
             if (
-                    self.superset_app.debug
-                    or self.superset_app.config["TESTING"]
-                    or is_test()
+                self.superset_app.debug
+                or self.superset_app.config["TESTING"]
+                or is_test()
             ):
                 logger.warning("Debug mode identified with default secret key")
                 log_default_secret_key_warning()
@@ -566,7 +566,7 @@ class SupersetAppInitializer:  # pylint: disable=too-many-public-methods
                     self.app = app
 
                 def __call__(
-                        self, environ: dict[str, Any], start_response: Callable[..., Any]
+                    self, environ: dict[str, Any], start_response: Callable[..., Any]
                 ) -> Any:
                     # Setting wsgi.input_terminated tells werkzeug.wsgi to ignore
                     # content-length and read the stream till the end.
@@ -599,13 +599,13 @@ class SupersetAppInitializer:  # pylint: disable=too-many-public-methods
 
         show_csp_warning = False
         if (
-                csp_warning
-                and not self.superset_app.debug
-                and (
+            csp_warning
+            and not self.superset_app.debug
+            and (
                 not talisman_enabled
                 or not talisman_config
                 or not talisman_config.get("content_security_policy")
-        )
+            )
         ):
             show_csp_warning = True
 
